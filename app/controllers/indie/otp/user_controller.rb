8cadae94613b68148bf0a90dd2508be35c6c5592
yottanami <yottanami@gmail.com>
2018-05-26 11:01:16 +0430

    login process modified

--- app/controllers/indie/otp/user_controller.rb
+++ app/controllers/indie/otp/user_controller.rb
@@ -2,14 +2,13 @@ require_dependency "indie/otp/application_controller"
 
 module Indie::Otp
   class UserController < ApplicationController
-    def register
-    end
 
     def request_otp
     end
 
     # Send OTP
-    def login
+    def generate_otp
+      ## SKIP IF USER IS LOGINNED
       mobile = user_params[:mobile]
       unless mobile.nil?
         user = User.find_by mobile: mobile
@@ -17,20 +16,50 @@ module Indie::Otp
           flash[:danger] = "User not found"
           redirect_to user_request_otp_path
         else
-          SMSModule.send(mobile, user.generate_otp)
+          otp_message = "Your Code is #{user.otp_code}"
+          sms_result = ::SMSModule.send(mobile, otp_message)
+          if  sms_result == true
+            session[:current_otp_id] = user.id
+   redirect_to user_check_otp_path
+          else
+            flash[:danger] = "Can not send text message!"
+            redirect_to user_request_otp_path
+          end
         end
       end
     end
 
-    def logout
+    def check_otp
+      otp = user_params[:otp]
+      unless otp.nil?
+        user = User.find_by id: session[:current_otp_id]
+        unless user.nil?
+          if user.authenticate_otp(otp)
+            cookies[:current_user_id] = user.id
+            flash[:success] = "Welcome back!"
+          else
+            flash[:danger] = "Wrong OTP"
+            redirect_to user_request_otp_path
+          end
+        else
+          reset_session
+          flash[:danger] = "User info did not send!"
+          redirect_to user_request_otp_path
+        end
+      else
+         flash[:danger] = "OTP info did not send!"
+         redirect_to user_generate_otp_path
+      end
     end
 
     def curent_user
+
     end
 
     private
     def user_params
-      params.permit(:mobile)
+      params.permit(:mobile, :otp)
     end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Indie::Otp::Engine.routes.draw do
   get 'user/register'
   get 'user/request_otp'
-  post 'user/login'
+  post 'user/generate_otp'
+  post 'user/check_otp'
   get 'user/logout'
 end
