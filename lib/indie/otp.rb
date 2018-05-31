d9e7d11f60977fc8f22d98c23fc084861e352c7d
yottanami <yottanami@gmail.com>
2018-05-31 13:45:00 +0430

    authentication methods modified

--- lib/indie/otp.rb
+++ lib/indie/otp.rb
@@ -3,9 +3,17 @@ require "indie/otp/engine"
 module Indie
   module Otp
     extend ActiveSupport::Concern
+    def user_signed_in?
+      not current_user.nil?
+    end
 
-     def current_user
-          puts "test output"
-     end
+    def current_user
+      User.find_by id: session['user']
+    end
+
+    def authenticate!
+      redirect_to user_request_otp_path unless user_signed_in?
+    end
+
   end
 end
