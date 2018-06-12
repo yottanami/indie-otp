12857c198eee4225ca4dd6b3f142b5da478d6250
yottanami <yottanami@gmail.com>
2018-06-12 14:21:22 +0430

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 IndieOtp::Engine.routes.draw do
+   get 'user/home'
+  get 'user/register'
+  get 'user/request_otp'
+  get 'user/generate_otp'
+  post 'user/generate_otp'
+  post 'user/check_otp'
+  get 'user/logout'
 end
